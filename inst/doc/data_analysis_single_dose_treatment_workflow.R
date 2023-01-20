e01ce28ed2f0ab5cb34d1d2ddead4faea08c7ac7
Jan-Philipp Quast <quast@imsb.biol.ethz.ch>
2023-01-20 09:30:02 +0000

    version 0.6.0

--- inst/doc/data_analysis_single_dose_treatment_workflow.R
+++ inst/doc/data_analysis_single_dose_treatment_workflow.R
@@ -6,180 +6,180 @@ knitr::opts_chunk$set(
 )
 
 ## ----setup, eval = test_protti, message = FALSE, warning = FALSE--------------
-library(protti)
-library(magrittr)
-library(dplyr)
+#  library(protti)
+#  library(magrittr)
+#  library(dplyr)
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  # To read in your own data you can use read_protti()
 #  your_data <- read_protti(filename = "mydata/data.csv")
 
 ## ----load_data, eval = test_protti--------------------------------------------
-data("rapamycin_10uM")
+#  data("rapamycin_10uM")
 
 ## ----median_normalisation_filtering, eval = test_protti, message = FALSE, warning = FALSE----
-data_normalised <- rapamycin_10uM %>%
-  filter(eg_is_decoy == FALSE) %>%
-  mutate(intensity_log2 = log2(fg_quantity)) %>%
-  normalise(sample = r_file_name, 
-            intensity_log2 = intensity_log2,
-            method = "median")
-
-data_filtered <- data_normalised %>%
-  filter_cv(grouping = eg_precursor_id, 
-            condition = r_condition, 
-            log2_intensity = intensity_log2, 
-            cv_limit = 0.25,
-            min_conditions = 1)
+#  data_normalised <- rapamycin_10uM %>%
+#    filter(eg_is_decoy == FALSE) %>%
+#    mutate(intensity_log2 = log2(fg_quantity)) %>%
+#    normalise(sample = r_file_name,
+#              intensity_log2 = intensity_log2,
+#              method = "median")
+#  
+#  data_filtered <- data_normalised %>%
+#    filter_cv(grouping = eg_precursor_id,
+#              condition = r_condition,
+#              log2_intensity = intensity_log2,
+#              cv_limit = 0.25,
+#              min_conditions = 1)
 
 ## ----data_preparation_prot_pep, eval = test_protti, message = FALSE, warning = FALSE----
-data_filtered_proteotypic <- data_filtered %>%
-  filter(pep_is_proteotypic == TRUE)
+#  data_filtered_proteotypic <- data_filtered %>%
+#    filter(pep_is_proteotypic == TRUE)
 
 ## ----fetching_database_information, eval = test_protti, message = FALSE, warning = FALSE----
-uniprot_ids <- unique(data_filtered_proteotypic$pg_protein_accessions)
-
-uniprot <-
-  fetch_uniprot(
-    uniprot_ids = uniprot_ids,
-    columns = c(
-      "protein_name",
-      "gene_names",
-      "go_f",
-      "xref_string",
-      "cc_interaction",
-      "ft_act_site",
-      "ft_binding",
-      "xref_pdb",
-      "length",
-      "sequence"
-    )
-  ) %>% 
-  rename(pg_protein_accessions = accession)
-
-data_filtered_uniprot <- data_filtered_proteotypic %>%
-  left_join(y = uniprot,
-            by = "pg_protein_accessions") %>%
-  find_peptide(protein_sequence = sequence,
-               peptide_sequence = pep_stripped_sequence) %>%
-  assign_peptide_type(aa_before = aa_before,
-               last_aa = last_aa, 
-               aa_after = aa_after) %>%
-  calculate_sequence_coverage(protein_sequence = sequence,
-                    peptides = pep_stripped_sequence)
+#  uniprot_ids <- unique(data_filtered_proteotypic$pg_protein_accessions)
+#  
+#  uniprot <-
+#    fetch_uniprot(
+#      uniprot_ids = uniprot_ids,
+#      columns = c(
+#        "protein_name",
+#        "gene_names",
+#        "go_f",
+#        "xref_string",
+#        "cc_interaction",
+#        "ft_act_site",
+#        "ft_binding",
+#        "xref_pdb",
+#        "length",
+#        "sequence"
+#      )
+#    ) %>%
+#    rename(pg_protein_accessions = accession)
+#  
+#  data_filtered_uniprot <- data_filtered_proteotypic %>%
+#    left_join(y = uniprot,
+#              by = "pg_protein_accessions") %>%
+#    find_peptide(protein_sequence = sequence,
+#                 peptide_sequence = pep_stripped_sequence) %>%
+#    assign_peptide_type(aa_before = aa_before,
+#                 last_aa = last_aa,
+#                 aa_after = aa_after) %>%
+#    calculate_sequence_coverage(protein_sequence = sequence,
+#                      peptides = pep_stripped_sequence)
 
 ## ----coverage_plot, eval = test_protti, fig.align= "center", fig.width = 6, fig.height = 5----
-qc_sequence_coverage(
-  data = data_filtered_uniprot,
-  protein_identifier = pg_protein_accessions,
-  coverage = coverage
-)
+#  qc_sequence_coverage(
+#    data = data_filtered_uniprot,
+#    protein_identifier = pg_protein_accessions,
+#    coverage = coverage
+#  )
 
 ## ----t_test, eval = test_protti, message = FALSE, warning = FALSE-------------
-diff_abundance_data <- data_filtered_uniprot %>%
-  assign_missingness(
-    sample = r_file_name,
-    condition = r_condition,
-    grouping = eg_precursor_id,
-    intensity = normalised_intensity_log2,
-    ref_condition = "control",
-    completeness_MAR = 0.7,
-    completeness_MNAR = 0.25,
-    retain_columns = c(pg_protein_accessions, 
-                       go_f, 
-                       xref_string, 
-                       start, 
-                       end, 
-                       length, 
-                       coverage)
-  ) %>%
-  calculate_diff_abundance(
-    sample = r_file_name,
-    condition = r_condition,
-    grouping = eg_precursor_id,
-    intensity_log2 = normalised_intensity_log2,
-    missingness = missingness,
-    comparison = comparison,
-    method = "moderated_t-test",
-    retain_columns = c(pg_protein_accessions, 
-                       go_f, 
-                       xref_string, 
-                       start, 
-                       end, 
-                       length, 
-                       coverage)
-  ) 
+#  diff_abundance_data <- data_filtered_uniprot %>%
+#    assign_missingness(
+#      sample = r_file_name,
+#      condition = r_condition,
+#      grouping = eg_precursor_id,
+#      intensity = normalised_intensity_log2,
+#      ref_condition = "control",
+#      completeness_MAR = 0.7,
+#      completeness_MNAR = 0.25,
+#      retain_columns = c(pg_protein_accessions,
+#                         go_f,
+#                         xref_string,
+#                         start,
+#                         end,
+#                         length,
+#                         coverage)
+#    ) %>%
+#    calculate_diff_abundance(
+#      sample = r_file_name,
+#      condition = r_condition,
+#      grouping = eg_precursor_id,
+#      intensity_log2 = normalised_intensity_log2,
+#      missingness = missingness,
+#      comparison = comparison,
+#      method = "moderated_t-test",
+#      retain_columns = c(pg_protein_accessions,
+#                         go_f,
+#                         xref_string,
+#                         start,
+#                         end,
+#                         length,
+#                         coverage)
+#    )
 
 ## ----pval_distribution, eval = test_protti, message = FALSE, warning = FALSE, fig.align= "center", fig.width = 6, fig.height = 5----
-pval_distribution_plot(data = diff_abundance_data,
-                       grouping = eg_precursor_id,
-                       pval = pval
-                       )
+#  pval_distribution_plot(data = diff_abundance_data,
+#                         grouping = eg_precursor_id,
+#                         pval = pval
+#                         )
 
 ## ----volcano_plot, eval = test_protti, fig.align= "center", fig.width = 6, fig.height = 5, message = FALSE, warning = FALSE----
-volcano_plot(
-  data = diff_abundance_data,
-  grouping = eg_precursor_id,
-  log2FC = diff,
-  significance = pval,
-  method = "target",
-  target_column = pg_protein_accessions,
-  target = "P62942",
-  x_axis_label = "log2(fold change) Rapamycin treated vs. untreated",
-  significance_cutoff = c(0.05, "adj_pval") 
-)
-
-# The significance_cutoff argument can also just be used for a 
-# regular cutoff line by just providing the cutoff value, e.g.
-# signficiance_cutoff = 0.05
+#  volcano_plot(
+#    data = diff_abundance_data,
+#    grouping = eg_precursor_id,
+#    log2FC = diff,
+#    significance = pval,
+#    method = "target",
+#    target_column = pg_protein_accessions,
+#    target = "P62942",
+#    x_axis_label = "log2(fold change) Rapamycin treated vs. untreated",
+#    significance_cutoff = c(0.05, "adj_pval")
+#  )
+#  
+#  # The significance_cutoff argument can also just be used for a
+#  # regular cutoff line by just providing the cutoff value, e.g.
+#  # signficiance_cutoff = 0.05
 
 ## ----barcode_plot, eval = test_protti, fig.align = "center", fig.width = 6, message = FALSE, warning = FALSE----
-FKBP12 <- diff_abundance_data %>%
-  filter(pg_protein_accessions == "P62942")
-
-barcode_plot(
-  data = FKBP12,
-  start_position = start,
-  end_position = end,
-  protein_length = length,
-  coverage = coverage,
-  colouring = diff,
-  cutoffs = c(diff = 1, adj_pval = 0.05),
-  protein_id = pg_protein_accessions
-  )
+#  FKBP12 <- diff_abundance_data %>%
+#    filter(pg_protein_accessions == "P62942")
+#  
+#  barcode_plot(
+#    data = FKBP12,
+#    start_position = start,
+#    end_position = end,
+#    protein_length = length,
+#    coverage = coverage,
+#    colouring = diff,
+#    cutoffs = c(diff = 1, adj_pval = 0.05),
+#    protein_id = pg_protein_accessions
+#    )
 
 ## ----woods_plot, eval = test_protti, fig.align = "center", fig.width = 6, message = FALSE, warning = FALSE----
-
-FKBP12 <- FKBP12 %>%
-  mutate(significant = ifelse(adj_pval < 0.01, TRUE, FALSE))
-
-woods_plot(
-  data = FKBP12,
-  fold_change = diff,
-  start_position = start,
-  end_position = end,
-  protein_length = length,
-  coverage = coverage,
-  colouring = adj_pval,
-  protein_id = pg_protein_accessions, 
-  facet = FALSE,
-  fold_change_cutoff = 1,
-  highlight = significant
-  )
+#  
+#  FKBP12 <- FKBP12 %>%
+#    mutate(significant = ifelse(adj_pval < 0.01, TRUE, FALSE))
+#  
+#  woods_plot(
+#    data = FKBP12,
+#    fold_change = diff,
+#    start_position = start,
+#    end_position = end,
+#    protein_length = length,
+#    coverage = coverage,
+#    colouring = adj_pval,
+#    protein_id = pg_protein_accessions,
+#    facet = FALSE,
+#    fold_change_cutoff = 1,
+#    highlight = significant
+#    )
 
 ## ----protile_plot, eval = test_protti, fig.align = "center", fig.width = 20, fig.height = 6, message = FALSE, warning = FALSE----
-FKBP12_intensity <- data_filtered_uniprot %>% 
-  filter(pg_protein_accessions == "P62942")
-
-peptide_profile_plot(
-  data = FKBP12_intensity,
-  sample = r_file_name,
-  peptide = eg_precursor_id,
-  intensity_log2 = normalised_intensity_log2,
-  grouping = pg_protein_accessions,
-  targets = "P62942",
-  protein_abundance_plot = FALSE
-)
+#  FKBP12_intensity <- data_filtered_uniprot %>%
+#    filter(pg_protein_accessions == "P62942")
+#  
+#  peptide_profile_plot(
+#    data = FKBP12_intensity,
+#    sample = r_file_name,
+#    peptide = eg_precursor_id,
+#    intensity_log2 = normalised_intensity_log2,
+#    grouping = pg_protein_accessions,
+#    targets = "P62942",
+#    protein_abundance_plot = FALSE
+#  )
 
 ## ----additional_functions, eval=FALSE-----------------------------------------
 #  diff_abundance_significant <- diff_abundance_data %>%
--- inst/doc/quality_control_workflow.R
+++ inst/doc/quality_control_workflow.R
@@ -6,157 +6,167 @@ knitr::opts_chunk$set(
 )
 
 ## ----setup, eval = test_protti, message = FALSE, warning = FALSE--------------
-library(protti)
-library(magrittr)
-library(dplyr)
-
+#  library(protti)
+#  library(magrittr)
+#  library(dplyr)
+#  
 
 ## ----create_synthetic_data, eval = test_protti--------------------------------
-# by setting the seed we are making sure that the random object generation can be reproduced
-set.seed(123) 
-
-data <- create_synthetic_data(n_proteins = 100,
-                              frac_change = 0.05,
-                              n_replicates = 3,
-                              n_conditions = 2,
-                              method = "effect_random",
-                              additional_metadata = TRUE)
-
+#  # by setting the seed we are making sure that the random object generation can be reproduced
+#  set.seed(123)
+#  
+#  data <- create_synthetic_data(n_proteins = 100,
+#                                frac_change = 0.05,
+#                                n_replicates = 3,
+#                                n_conditions = 2,
+#                                method = "effect_random",
+#                                additional_metadata = TRUE)
+#  
 
 ## ----qc_cvs, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-input <- data %>%
-  # as the data is log2 transformed, we need to transform it back before calculating the CVs
-  mutate(raw_intensity = 2^peptide_intensity_missing) 
-
-qc_cvs(data = input,
-       grouping = peptide,
-       condition = condition,
-       intensity = raw_intensity, 
-       plot = FALSE)
-
-qc_cvs(data = input,
-       grouping = peptide,
-       condition = condition,
-       intensity = raw_intensity, 
-       plot = TRUE, 
-       plot_style = "violin")
+#  input <- data %>%
+#    # as the data is log2 transformed, we need to transform it back before calculating the CVs
+#    mutate(raw_intensity = 2^peptide_intensity_missing)
+#  
+#  qc_cvs(data = input,
+#         grouping = peptide,
+#         condition = condition,
+#         intensity = raw_intensity,
+#         plot = FALSE)
+#  
+#  qc_cvs(data = input,
+#         grouping = peptide,
+#         condition = condition,
+#         intensity = raw_intensity,
+#         plot = TRUE,
+#         plot_style = "violin")
 
 ## ----qc_ids, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_ids(data = input,
-       sample = sample,
-       grouping = protein,
-       intensity = peptide_intensity_missing,
-       condition = condition, 
-       plot = FALSE)
-
-qc_ids(data = input,
-       sample = sample,
-       grouping = protein,
-       intensity = peptide_intensity_missing,
-       condition = condition, 
-       title = "Protein identifications per sample",
-       plot = TRUE)
+#  qc_ids(data = input,
+#         sample = sample,
+#         grouping = protein,
+#         intensity = peptide_intensity_missing,
+#         condition = condition,
+#         plot = FALSE)
+#  
+#  qc_ids(data = input,
+#         sample = sample,
+#         grouping = protein,
+#         intensity = peptide_intensity_missing,
+#         condition = condition,
+#         title = "Protein identifications per sample",
+#         plot = TRUE)
 
 ## ----qc_peptide_type, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_peptide_type(data = input,
-                sample = sample, 
-                peptide = peptide, 
-                pep_type = pep_type, 
-                method = "intensity", 
-                intensity = raw_intensity, 
-                plot = TRUE, 
-                interactive = FALSE)
-
-qc_peptide_type(data = input,
-                sample = sample, 
-                peptide = peptide, 
-                pep_type = pep_type, 
-                method = "count", 
-                plot = TRUE, 
-                interactive = FALSE)
+#  qc_peptide_type(data = input,
+#                  sample = sample,
+#                  peptide = peptide,
+#                  pep_type = pep_type,
+#                  method = "intensity",
+#                  intensity = raw_intensity,
+#                  plot = TRUE,
+#                  interactive = FALSE)
+#  
+#  qc_peptide_type(data = input,
+#                  sample = sample,
+#                  peptide = peptide,
+#                  pep_type = pep_type,
+#                  method = "count",
+#                  plot = TRUE,
+#                  interactive = FALSE)
 
 ## ----qc_intensity_distribution_boxplot, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-
-qc_intensity_distribution(data = input,
-                          sample = sample,
-                          grouping = peptide,
-                          intensity_log2 = peptide_intensity_missing,
-                          plot_style = "boxplot")
-
-qc_median_intensities(data = input,
-                      sample = sample,
-                      grouping = peptide,
-                      intensity = peptide_intensity_missing)
+#  
+#  qc_intensity_distribution(data = input,
+#                            sample = sample,
+#                            grouping = peptide,
+#                            intensity_log2 = peptide_intensity_missing,
+#                            plot_style = "boxplot")
+#  
+#  qc_median_intensities(data = input,
+#                        sample = sample,
+#                        grouping = peptide,
+#                        intensity = peptide_intensity_missing)
 
 ## ----qc_charge_states, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_charge_states(data = input, 
-                 sample = sample, 
-                 grouping = peptide, 
-                 charge_states = charge, 
-                 method = "intensity",
-                 intensity = raw_intensity, 
-                 plot = TRUE)
+#  qc_charge_states(data = input,
+#                   sample = sample,
+#                   grouping = peptide,
+#                   charge_states = charge,
+#                   method = "intensity",
+#                   intensity = raw_intensity,
+#                   plot = TRUE)
 
 ## ----qc_missed_cleavages, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_missed_cleavages(data = input, 
-                    sample = sample, 
-                    grouping = peptide, 
-                    missed_cleavages = n_missed_cleavage, 
-                    method = "intensity",
-                    intensity = raw_intensity, 
-                    plot = TRUE)
+#  qc_missed_cleavages(data = input,
+#                      sample = sample,
+#                      grouping = peptide,
+#                      missed_cleavages = n_missed_cleavage,
+#                      method = "intensity",
+#                      intensity = raw_intensity,
+#                      plot = TRUE)
 
 ## ----qc_sequence_coverage, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_sequence_coverage(data = input, 
-                     protein_identifier = protein, 
-                     coverage = coverage)
+#  qc_sequence_coverage(data = input,
+#                       protein_identifier = protein,
+#                       coverage = coverage)
 
 ## ----qc_peak_width, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_peak_width(data = input,
-              sample = sample, 
-              intensity = peptide_intensity_missing,
-              retention_time = retention_time, 
-              peak_width = peak_width)
+#  qc_peak_width(data = input,
+#                sample = sample,
+#                intensity = peptide_intensity_missing,
+#                retention_time = retention_time,
+#                peak_width = peak_width)
 
 ## ----qc_data_completeness, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_data_completeness(data = input, 
-                     sample = sample, 
-                     grouping = peptide, 
-                     intensity = peptide_intensity_missing, 
-                     plot = TRUE)
+#  qc_data_completeness(data = input,
+#                       sample = sample,
+#                       grouping = peptide,
+#                       intensity = peptide_intensity_missing,
+#                       plot = TRUE)
 
 ## ----qc_intensity_distribution_histogram, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_intensity_distribution(data = input,
-                          grouping = peptide,
-                          intensity_log2 = peptide_intensity_missing,
-                          plot_style = "histogram")
+#  qc_intensity_distribution(data = input,
+#                            grouping = peptide,
+#                            intensity_log2 = peptide_intensity_missing,
+#                            plot_style = "histogram")
 
 ## ----qc_sample_correlation, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
- qc_sample_correlation(data = input,
-                       sample = sample, 
-                       grouping = peptide, 
-                       intensity_log2 = peptide_intensity_missing, 
-                       condition = condition, 
-                       interactive = FALSE)
+#   qc_sample_correlation(data = input,
+#                         sample = sample,
+#                         grouping = peptide,
+#                         intensity_log2 = peptide_intensity_missing,
+#                         condition = condition,
+#                         interactive = FALSE)
 
 ## ----qc_pca, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
-qc_pca(
-  data = data,
-  sample = sample,
-  grouping = peptide,
-  intensity = peptide_intensity_missing,
-  condition = condition,
-  digestion = NULL,
-  plot_style = "scree"
-)
+#  qc_pca(
+#    data = data,
+#    sample = sample,
+#    grouping = peptide,
+#    intensity = peptide_intensity_missing,
+#    condition = condition,
+#    digestion = NULL,
+#    plot_style = "scree"
+#  )
+#  
+#  qc_pca(
+#    data = data,
+#    sample = sample,
+#    grouping = peptide,
+#    intensity = peptide_intensity_missing,
+#    condition = condition,
+#    components = c("PC1", "PC2"),
+#    plot_style = "pca"
+#  )
 
-qc_pca(
-  data = data,
-  sample = sample,
-  grouping = peptide,
-  intensity = peptide_intensity_missing,
-  condition = condition,
-  components = c("PC1", "PC2"), 
-  plot_style = "pca"
-)
+## ----qc_ranked_intensities, eval = test_protti, fig.width = 6, fig.height = 4, fig.align = "center"----
+#  # Plot ranked peptide intensities
+#  qc_ranked_intensities(
+#    data = data,
+#    sample = sample,
+#    grouping = peptide,
+#    intensity_log2 = peptide_intensity,
+#    plot = TRUE,
+#  )
 
